4360ba5626fee87122ada03af8f17d2d0d39ffbb
lbedo1200 <37681125+lbedo1200@users.noreply.github.com>
2021-07-21 22:13:19 -0700

    Update control_panel.ino
    Added logic for a panel sounder
    Updated variable names to better reflect panel labels
    Updated comments

--- control_panel.ino
+++ control_panel.ino
@@ -3,7 +3,7 @@ const int Dispatch = 0;
 int DispatchPOS = 0;
 const int EStopButton = 1;
 int EStopPOS = 0;
-const int ResetButton = 2;
+const int AcknowledgeButton = 2;
 int ResetPOS = 0;
 const int OPEnable = 3;
 int OPEnablePOS = 0;
@@ -12,9 +12,9 @@ int RestraintsPOS = 0;
 const int Gates = 5;
 int GatesPOS = 0;
 
-const int ResetLight = 12;
+const int AcknowledgeLight = 12;
 const int EStopLight = 13;
-const int Alarm = 10;
+const int Alarm = 11;
 
 bool fault = false;
 unsigned long resetTimerStart = 0;
@@ -25,17 +25,17 @@ int lastGPOS = 0;
 
 void setup()
 {
-  pinMode(Dispatch, INPUT_PULLUP);
-  pinMode(EStopButton, INPUT_PULLUP);
-  pinMode(ResetButton, INPUT_PULLUP);
+  pinMode(Dispatch, INPUT_PULLUP);          //Button that dispatches train when safe
+  pinMode(EStopButton, INPUT_PULLUP);       //Button that stops trains on the next available block and stop the lift
+  pinMode(AcknowledgeButton, INPUT_PULLUP); //Button that acknowledges fault and clears it
   
-  pinMode(OPEnable, INPUT_PULLUP);
-  pinMode(Restraints, INPUT_PULLUP);
-  pinMode(Gates, INPUT_PULLUP);
+  pinMode(OPEnable, INPUT_PULLUP);          //Operator enable switch that enables the dispatch button
+  pinMode(Restraints, INPUT_PULLUP);        //Restraints switch that opens and closes restraints
+  pinMode(Gates, INPUT_PULLUP);             //Entrance Gate switch that opens and closes gates
   
-  pinMode(ResetLight, OUTPUT);
-  pinMode(EStopLight, OUTPUT);
-  pinMode(Alarm, OUTPUT);
+  pinMode(AcknowledgeLight, OUTPUT);        //Light on Acknowlegde button that turns on when button is pressed
+  pinMode(EStopLight, OUTPUT);              //Light on E-Stop button that turns on when pressed and blinks when released
+  pinMode(Alarm, OUTPUT);                   //Alarm sounder that beeps when system is faulted
   Keyboard.begin();
 }
 
@@ -44,7 +44,7 @@ void loop()
   //Variables with the button positions (HIGH = Unpressed, LOW = Pressed)
   EStopPOS = digitalRead(EStopButton);
   DispatchPOS = digitalRead(Dispatch);
-  ResetPOS = digitalRead(ResetButton);
+  ResetPOS = digitalRead(AcknowledgeButton);
   GatesPOS = digitalRead(Gates);
   RestraintsPOS = digitalRead(Restraints);
   OPEnablePOS = digitalRead(OPEnable);
@@ -52,7 +52,7 @@ void loop()
   //Resets dispatch, Estop, and reset buttons. Could probably be done better
   ResetButtons();
   
-  //EStop Button: Finished?(Test with actual lights)
+  //EStop Button logic
   //If pushed, Light up button and set fault to true if not already
   //Else, Turn off light if no fault
   if(EStopPOS == LOW)
@@ -69,28 +69,28 @@ void loop()
   }
 
   
-  //Fault Light: FINISHED? (Test with actual lights)
-  //Makes EStop light blink if button is pulled out, but there's a fault 
+  //Fault Light
+  //Makes EStop light blink if eStop button is pulled out and there's a fault 
   if(EStopPOS == HIGH && fault == true)
       FaultLight();
 
-  //Reset Button: Finished? (Test with actual lights)
+  //Reset Button
   //Turns on Reset Light if pushed in, else turns off
   //calls Reset function if pressed and there's a fault
   if(ResetPOS == LOW)
   {
-    digitalWrite(ResetLight, HIGH);
+    digitalWrite(AcknowledgeLight, HIGH);
     if(fault == true)
       Reset();
   }
   else
   {
-    digitalWrite(ResetLight, LOW);
+    digitalWrite(AcknowledgeLight, LOW);
     resetTimerStart = 0;
   }
 
 
-  //Air Gates:Finished
+  //Logic for Entrance Gate Switch
   //Presses 9 on keyboard if switched on
   //Presses 3 on keyboard if switched off
   if(GatesPOS != lastGPOS)
@@ -108,7 +108,7 @@ void loop()
   }
   lastGPOS = GatesPOS;
 
-  //Restraints:Finished
+  //Restraint switch logic
   //Presses 8 on keyboard if switched on
   //Presses 2 on keyboard if switched off
   if(RestraintsPOS != lastRPOS)
@@ -126,31 +126,30 @@ void loop()
   }
   lastRPOS = RestraintsPOS;
   
-  //Dispatch: Finished
-  //If button is pressed, there is no fault, and the restraints and gates are closed, press enter on the keyboard
+  //Dispatch button logic
+  //If button is pressed, there is no fault, and the restraints and gates are both closed, press enter on the keyboard
   if (DispatchPOS == LOW && fault != true && OPEnablePOS == LOW && GatesPOS == HIGH && RestraintsPOS == HIGH)
   {
     Keyboard.press(176);
   } 
 }
 
-//Presses and Holds F8 on keyboard, turns on the alarm, and sets fault to true if called
+//Presses and Holds F8 on keyboard and sets fault to true if called
 void EStop()
 {
   Keyboard.press(KEY_F8);
-  digitalWrite(Alarm, HIGH);
   fault = true;
 }
 
-//More complicated than it should be
-//If reset button is pressed for 2 seconds, set fault to false, and let go of F8
+
+//If acknowlege button is pressed for 2 seconds, set fault to false, and let go of F8
 void Reset()
 {
   if (resetTimerStart == 0)
   {
     resetTimerStart = millis();
   }
-  digitalWrite(ResetLight, HIGH);
+  digitalWrite(AcknowledgeLight, HIGH);
     
   if (millis() > resetTimerStart + 2000)
   { 
@@ -160,7 +159,6 @@ void Reset()
   }
 }
 
-//More complicated than it should be
 //Makes EStop button blink, but allows it to be interupted
 void FaultLight()
 {
@@ -170,22 +168,24 @@ void FaultLight()
     previousMillis = currentMillis;
     if (digitalRead(EStopLight) == LOW)
     {
+      digitalWrite (Alarm, HIGH);
       digitalWrite(EStopLight, HIGH);
     }
     else
     {
+      digitalWrite(Alarm, LOW);
       digitalWrite(EStopLight, LOW);
     }
   }
 }
 
-//Probably get rid of this function later
+
 //Releases the enter key if button is let go
 //Releases F8 key if estop and reset buttons aren't pressed
 void ResetButtons()
 {
   if (digitalRead(Dispatch) == HIGH)
     Keyboard.release(176);
-  if (digitalRead(ResetButton) == HIGH && digitalRead(EStopButton) == HIGH)
+  if (digitalRead(AcknowledgeButton) == HIGH && digitalRead(EStopButton) == HIGH)
     Keyboard.release(KEY_F8);
 }
